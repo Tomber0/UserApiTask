3ede481c7fa3b497e48b0dd42d8bfc268f1c5fed
Aleksey.Zaitsau <aleseyzaicev@gmail.com>
2023-10-11 06:04:53 +0300

    added migrations

--- UserApiTask/Models/AppDbContext.cs
+++ UserApiTask/Models/AppDbContext.cs
@@ -9,7 +9,7 @@ namespace UserApiTask.Models
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         {
-            //Database.EnsureDeleted();
+            Database.EnsureDeleted();
             Database.EnsureCreated();
         }
 
@@ -20,24 +20,8 @@ namespace UserApiTask.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //base.OnModelCreating(modelBuilder);
-            var role1 = new Role()
-            {
-                Id = 1,
-                Name = "User"
-            };
-            var role2 = new Role()
-            {
-                Id = 2,
-
-                Name = "Admin"
-            };
-            modelBuilder.Entity<Role>().HasData(role1,role2, new Role() { Id = 3, Name = "Support" }, new Role() { Id = 4, Name = "SuperAdmin" });
-
-            modelBuilder.Entity<User>().HasData(new User() { Id = 1, Name = "User", Age = 22, Email = "user@mail.com"});
-            modelBuilder.Entity<User>().HasData(new User() { Id = 2, Name = "User2", Age = 22, Email = "user2@mail.com"});
-            modelBuilder.Entity<User>().HasData(new User() { Id = 3, Name = "User3", Age = 22, Email = "user3@mail.com" });
-
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Role>().HasData(new Role() { Id = 1, Name = "User"}, new Role(){Id = 2,Name = "Admin"}, new Role() { Id = 3, Name = "Support" }, new Role() { Id = 4, Name = "SuperAdmin" });
         }
     }
 }
